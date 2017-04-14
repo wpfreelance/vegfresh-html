35475a9224901c2620e391c13615d9b5a99b2719
Pradipta Sinha <singh.gabu@gmail.com>
2017-04-14 15:07:20 +0530

    initial commit
    initial commit

---
+++ js/style.js
@@ -0,0 +1,32 @@
+/*jQuery('.custom1').owlCarousel({
+    animateOut: 'slideOutDown',
+    animateIn: 'flipInX',
+    items:1,
+    margin:30,
+    stagePadding:30,
+    smartSpeed:450
+});
+
+*/
+
+
+
+
+jQuery('.owl-carousel').owlCarousel({
+    //loop:true,
+    margin:0,
+    nav:false,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
